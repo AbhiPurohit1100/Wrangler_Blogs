d8a9684d5d13061e5aac56432e7b3cfde6c58b1d
AbhiPurohit1100 <abhinavpurohit30@gmail.com>
2025-07-01 02:25:37 +0530

    backend and zod validations

---
+++ backend/src/index.ts
@@ -0,0 +1,96 @@
+import { Hono } from 'hono'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
+import { PrismaClient } from './generated/prisma/edge'
+import { withAccelerate, WithAccelerateOptions } from '@prisma/extension-accelerate'
+import { sign, decode, verify } from 'hono/jwt'
+import { signinInput,createBlogInput, updateBlogInput} from '@abhiyuck/commonzod2'
+
+const app = new Hono<{
+  Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+      DIRECT_URL: string
+  }
+}>()
+
+app.route("/api/v1/user", userRouter)
+app.route("api/v1/blog", blogRouter)
+
+
+app.use('api/v1/post*', async (c, next)=>{
+  const header = c.res.header("Authorization") || "";
+  const token = header.split(" ")[1]
+  const response = await verify(token, c.env.JWT_SECRET)
+  if(response.id){
+    await next()
+
+  }else{
+    c.status(403)
+    return c.json({error: "unauthorized"})
+  }
+}) 
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+// app.post('/api/v1/signup' , async (c) =>{
+
+//   const prisma = new PrismaClient({
+//     datasourceUrl: c.env.DATABASE_URL,
+//   }).$extends(withAccelerate())
+//   try{
+//   const body = await c.req.json()
+//   const user = await prisma.user.create({
+//     data: {
+//       email: body.email,
+//       password: body.password,
+//       name: body.name
+//     }
+//   })
+//   const token = await sign({id: user.id}, c.env.JWT_SECRET)
+//   return c.json({
+//     jwt: token
+//   })
+// }
+
+//   catch(e){
+//     c.status(403)
+//   return c.json({
+//     "error": "invalid and already exist"
+//   })
+//   }
+  
+// })
+
+// app.post('/api/v1/signin' ,async (c) => {
+//   const prisma = new PrismaClient({
+//     //@ts-ignore
+//     datasourceUrl: c.env.DATABASE_URL,
+//   }).$extends(withAccelerate())
+
+//   const body = await c.req.json();
+//   const user = await prisma.user.findFirst({
+//     where : {
+//       email : body.email,
+//       password: body.password
+//     }
+// })
+  
+//   if(!user){
+//     c.status(403)
+//     return c.json({
+//       error: "not found in db"
+//     })
+//   }
+//   else{
+//     const jwt = await sign({id: user.id}, c.env.JWT_SECRET)
+//     return c.json({"jwt": jwt})
+//   }
+
+  
+// })
+
+
+export default app
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,85 @@
+import { Hono } from "hono"
+import { PrismaClient } from "../generated/prisma/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+
+import { signinInput,createBlogInput, updateBlogInput, signupInput} from '@abhiyuck/commonzod2'
+
+export const userRouter = new Hono<{
+  Bindings: {
+      DATABASE_URL: string;
+      JWT_SECRET: string;
+      DIRECT_URL: string
+  }
+}>()
+userRouter.post('/signup' , async (c) =>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  try{
+  const body = await c.req.json()
+  const {success} = signupInput.safeParse(body)
+
+  if(!success){
+    c.status(404)
+    return c.json({
+      msg: "input meathod not correct"
+    })
+  }
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+      name: body.name
+    }
+  })
+  const token = await sign({id: user.id}, c.env.JWT_SECRET)
+  return c.json({
+    jwt: token
+  })
+}
+
+  catch(e){
+    c.status(403)
+  return c.json({
+    "error": "invalid and already exist"
+  })
+  }
+  
+})
+
+userRouter.post('/signin' ,async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  const {success} = signinInput.safeParse(body)
+
+  if(!success){
+    c.status(404)
+    return c.json({
+      msg: "input meathod not correct"
+    })
+  }
+  const user = await prisma.user.findFirst({
+    where : {
+      email : body.email,
+      password: body.password
+    }
+})
+  
+  if(!user){
+    c.status(403)
+    return c.json({
+      error: "not found in db"
+    })
+  }
+  else{
+    const jwt = await sign({id: user.id}, c.env.JWT_SECRET)
+    return c.json({"jwt": jwt})
+  }
+
+  
+})
